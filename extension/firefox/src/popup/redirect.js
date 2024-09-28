63749982b07e4681aae19fa60bae5a514a83287f
Hisdri <40797757+hisdri@users.noreply.github.com>
2024-09-28 21:02:50 +0200

    Extension now redirects with only domain name in query

--- extension/firefox/src/popup/redirect.js
+++ extension/firefox/src/popup/redirect.js
@@ -1,9 +1,23 @@
-const scamHunterURL = "google.com/search?q=";
+const scamHunterURL = "https://google.com/search?q=";
 
+function getDomain(url) {
+    try {
+      // Create a URL object
+      const parsedUrl = new URL(url);
+      
+      // Return the protocol and hostname (domain)
+      return `${parsedUrl.protocol}//${parsedUrl.hostname}`;
+    } catch (e) {
+      // If the input is not a valid URL, return an error message or handle it
+      console.error('Invalid URL:', e);
+      return null;
+    }
+  }
+  
 browser.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
     // Get the URL of the active tab
     const activeTab = tabs[0];
-    const activeTabURL = activeTab.url;
+    const activeTabURL = getDomain(activeTab.url);
 
     // Create a tab with current url as query to Scam Hunter site
     browser.tabs.create({ url: scamHunterURL + activeTabURL });
